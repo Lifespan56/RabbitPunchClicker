e2369d52c5bf508c1e69f775f9e0849037c6ded8
Lifespan56 <lifespan56@gmail.com>
2024-11-12 20:45:45 +0900

    [update]Controllers
    input,Anim컨트롤러 추가
    중간세이브

---
+++ Assets/Scripts/BasicAttackType.cs
@@ -0,0 +1,8 @@
+﻿public enum BasicAttackType
+{
+    A,
+    B,
+    C,
+    D,
+    Count
+}
---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MonoBehaviour
+{
+    public event Action OnBasicAttack;
+    public void OnInput_LeftClick(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            OnBasicAttack?.Invoke();
+        }
+    }
+}
